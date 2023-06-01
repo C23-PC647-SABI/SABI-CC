32b09198d801e2ee7d05e9c571c7864dc2f4f771
Umy Namiroh <133233048+umyname@users.noreply.github.com>
2023-06-01 21:25:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ connection.connect(function(error){
      console.log('Koneksi Berhasil!');
    }
  })
+
+
  router.get('/',(req, res) => {
-    const query = `SELECT * FROM word`;
+    const query = "SELECT * FROM dictionary"
     connection.query(query, (err, rows, field) => {
         if(err) {
             res.status(500).send({message: err.sqlMessage})
@@ -35,11 +37,10 @@ connection.connect(function(error){
 })
 
 
-router.get("/getword/:id", (req, res) => {
-  const id = req.params.id
-
-  const query = "SELECT * FROM word WHERE word_id = ?"
-  connection.query(query, [id], (err, rows, field) => {
+router.get("/getWord", (req, res) => {
+  const word = req.query.word
+  const query = "SELECT * FROM dictionary WHERE word = ?"
+  connection.query(query,[word], (err, rows, field) => {
       if(err) {
           res.status(500).send({message: err.sqlMessage})
       } else {
@@ -48,12 +49,24 @@ router.get("/getword/:id", (req, res) => {
   })
 })
 
-router.get("/searchword", (req, res) => {
-  const s = req.query.s;
+router.get("/getDescription", (req, res) => {
+    const description = req.query.description
+    const query = "SELECT * FROM dictionary WHERE description = ?"
+    connection.query(query, [description], (err, rows, field) => {
+        if(err) {
+            res.status(500).send({message: err.sqlMessage})
+        } else {
+            res.json(rows)
+        }
+    })
+  })
 
-  console.log(s)
-  const query = "SELECT * FROM word WHERE name LIKE '%" + s + "%' or notes LIKE '%" + s + "%'"
-  connection.query(query, (err, rows, field) => {
+router.get("/searchWord", (req, res) => {
+  const word = req.query.word
+  const description = req.query.description
+
+  const query = "SELECT * FROM dictionary WHERE word = ? OR description = ?"
+  connection.query(query,[word , description], (err, rows, field) => {
       if(err) {
           res.status(500).send({message: err.sqlMessage})
       } else {
@@ -62,16 +75,30 @@ router.get("/searchword", (req, res) => {
   })
 })
 
-router.post("/insertword", multer.single('attachment'), imgUpload.uploadToGcs, (req, res) => {
+router.post('/insertWord',(req, res) => {
+    const word = req.query.word
+    const description = req.query.description
+
+    const query = "INSERT INTO dictionary (word, description) values(?, ?)"
+    connection.query(query, [word,description], (err, rows, field) => {
+        if(err) {
+            res.status(500).send({message: err.sqlMessage})
+        } else {
+            res.send({message: "Insert Successful"})
+        }
+    })
+})
+
+router.post("/insertWithImage", multer.single('attachment'), imgUpload.uploadToGcs, (req, res) => {
   const word = req.body.word
-  const description = req.body.amount
+  const description = req.body.description
   var imageUrl = ''
 
   if (req.file && req.file.cloudStoragePublicUrl) {
       imageUrl = req.file.cloudStoragePublicUrl
   }
 
-  const query = "INSERT INTO word (word, desription) values (?, ?)"
+  const query = "INSERT INTO dictionary (word, description, attachment) values (?, ?, ?)"
 
   connection.query(query, [word, description, imageUrl], (err, rows, fields) => {
       if (err) {
@@ -81,31 +108,26 @@ router.post("/insertword", multer.single('attachment'), imgUpload.uploadToGcs, (
       }
   })
 })
-router.put("/editword/:id", multer.single('attachment'), imgUpload.uploadToGcs, (req, res) => {
-  const word = req.body.word
-  const description = req.body.amount
-  var imageUrl = ''
 
-  if (req.file && req.file.cloudStoragePublicUrl) {
-      imageUrl = req.file.cloudStoragePublicUrl
-  }
 
-  const query = "UPDATE word SET word = ?, description = ?"
-  
-  connection.query(query, [word, description, imageUrl, id], (err, rows, fields) => {
-      if (err) {
-          res.status(500).send({message: err.sqlMessage})
-      } else {
-          res.send({message: "Update Successful"})
-      }
-  })
+router.put('/updateWord',(req, res) => {
+    const word= req.body.word
+
+    const query = "UPDATE dictionary SET word= ? WHERE word= ?"
+    connection.query(query,[word], (err, rows, field) => {
+        if(err) {
+            res.status(500).send({message: err.sqlMessage})
+        } else {
+            res.send({message: "Update successful"})
+        }
+    })
 })
 
-router.delete("/deleteword/:id", (req, res) => {
-  const id = req.params.id
-  
-  const query = "DELETE FROM word WHERE id = ?"
-  connection.query(query, [id], (err, rows, fields) => {
+router.delete("/deleteWord", (req, res) => {
+  const word = req.query.word
+
+  const query = "DELETE FROM dictionary WHERE word = ?"
+  connection.query(query,[word], (err, rows, fields) => {
       if (err) {
           res.status(500).send({message: err.sqlMessage})
       } else {
